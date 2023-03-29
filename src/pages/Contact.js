687177e21951a02fa470b808608e3e9db14135ca
Anastasiia Shevchuk <anastacia.boiko@gmail.com>
2023-03-28 20:39:33 -0700

    Add AC form

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,16 +1,29 @@
-import React from "react"
+import React from "react";
+import { ActiveCampaignInputs, handleActiveCampaignSubmit } from 'active-campaign-react';
+import { useForm } from 'react-hook-form';
 
 const Contact = () => {
+    const { register, handleSubmit } = useForm();
+    const formId = '_form_1_';
+
+    const onSubmit = async (data) => {
+        handleActiveCampaignSubmit(data, 'https://anastaciaboiko.activehosted.com/proc.php', formId)
+    }
+
     return (
         <div className="page">
             <h1>Contact</h1>
             <p>This is the contact page.</p>
 
-            <div>
-                <p>Subscribe to our newsletter:</p>
-                <input type="email"></input>
-                <button>Subscribe</button>
-            </div>
+            <p>Subscribe to our newsletter:</p>
+            <form className="newsletter-form" onSubmit={handleSubmit(onSubmit)}>
+                <ActiveCampaignInputs formId={formId} />
+
+                <input placeholder="Name" name="name" id="name" {...register('name', { required: true })} />
+                <input placeholder="Email" name="email" type="email" id="email" {...register('email', { required: true })} />
+
+                <button type="submit">Subscribe</button>
+            </form>
         </div>
     )
 }
